0329cc0d96c28b6453ef98ca9f113fed3a46a87a
rabakuku <71357873+rabakuku@users.noreply.github.com>
2022-02-20 12:47:37 -0500

    Create AutoPocketNet.sh

---
+++ AutoPocketNet.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "starting the installation"
+pause 2s
+
+echo "Installing the Ubuntu GUI"
+pause 2s
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install --no-install-recommends ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal -y
+
+
+echo "Downloading and installing .deb"
+pause 2s
+wget https://github.com/pocketnetteam/pocketnet.core/releases/download/v0.20.18/pocketnetcore_0.20.18_linux_x64_setup.deb
+sudo dpkg -i pocketnetcore_*_linux_x64_setup.deb
+
+echo "Making the directories"
+pause 2s
+mkdir ~/.pocketcoin
+mkdir ~/.pocketcoin/checkpoints
+cd ~/.pocketcoin/checkpoints
+wget https://github.com/pocketnetteam/pocketnet.core/releases/latest/download/main.sqlite3
+cd ~
+
+echo "dowloading and unziping the blockchain"
+pause 2s
+
+cd ~/.pocketcoin
+rm -r blocks
+rm -r chainstate
+rm -r indexes
+rm -r pocketdb
+wget https://snapshot.pocketnet.app/latest.tgz
+tar -xzf latest.tgz -C ./
+
+echo "completed"
+pause 2s
