d926bb2b7b85e6babd71773f6dc76699fa75dac0
rabakuku <71357873+rabakuku@users.noreply.github.com>
2022-03-07 21:57:02 -0500

    Update AutoPocketNet.sh

--- AutoPocketNet.sh
+++ AutoPocketNet.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
-echo "starting the installation"
-pause 2s
-
-echo "Installing the Ubuntu GUI"
-pause 2s
+read -t 3 -p "starting the installation"
 sudo apt-get update && sudo apt-get upgrade -y
 
-echo "Downloading and installing .deb"
-pause 2s
+
+read -t 3 -p "Downloading and installing .deb"
 wget https://github.com/pocketnetteam/pocketnet.core/releases/download/v0.20.18/pocketnetcore_0.20.18_linux_x64_setup.deb
 sudo dpkg -i pocketnetcore_*_linux_x64_setup.deb
 
-echo "Making the directories"
-pause 2s
+read -t 3 -p "Making the directories"
 mkdir /root/.pocketcoin
 mkdir /root/.pocketcoin/checkpoints
 cd /root/.pocketcoin/checkpoints
 wget https://github.com/pocketnetteam/pocketnet.core/releases/latest/download/main.sqlite3
 cd ~
 
-echo "downloading pocketcoin.conf"
+read -t 3 -p "downloading pocketcoin.conf"
 wget https://raw.githubusercontent.com/rabakuku/AutoPocketNet.Core/main/pocketcoin.conf -O ~/.pocketcoin/pocketcoin.conf
 
 
-echo "dowloading and unziping the blockchain"
-pause 2s
-cd /root/.pocketcoin
-rm -r blocks
-rm -r chainstate
-rm -r indexes
-rm -r pocketdb
+read -t 3 -p  "dowloading and unziping the blockchain"
+cd  /root/.pocketcoin/
+rm -r /root/.pocketcoin/blocks
+rm -r /root/.pocketcoin/chainstate
+rm -r /root/.pocketcoin/indexes
+rm -r /root/.pocketcoin/pocketdb
 wget https://snapshot.pocketnet.app/latest.tgz
 tar -xzf latest.tgz -C ./
 
-echo "rm latest.tgz"
-pause 2s
+read -t 3 -p "rm latest.tgz"
 rm /root/.pocketcoin/latest.tgz
 echo "completed"
 pause 2s
 
-echo "starting node"
-pause 2s
-pocketcoind
+read -t 3 -p "starting node"
+pocketcoin-qt
