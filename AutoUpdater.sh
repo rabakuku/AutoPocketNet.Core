4048c0269b499f99e5114399c80b644d71faa32f
rabakuku <71357873+rabakuku@users.noreply.github.com>
2022-03-20 21:05:24 -0400

    Create AutoUpdater.sh

---
+++ AutoUpdater.sh
@@ -0,0 +1,16 @@
+read -t 3 -p "Stopping old version"
+pocketcoin-cli stop
+
+read -t 3 -p "Deleting old version"
+rm pocketnetcore_0*
+
+read -t 20 -p "Downloading and installing new version"
+wget https://github.com/pocketnetteam/pocketnet.core/releases/download/v0.20.19/pocketnetcore_0.20.19_linux_x64_setup.deb --no-check-certificate
+sudo dpkg -i pocketnetcore_*_linux_x64_setup.deb
+
+
+read -t 20 -p "starting daemon with new version"
+pockecoind -daemon
+
+read -t 3 -p "verifying it is running"
+pocketcoin-cli getstakinginfo
