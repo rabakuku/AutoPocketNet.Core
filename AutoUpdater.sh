2162cf8000d6b0217edf205ec62e14c55b6160d1
rabakuku <71357873+rabakuku@users.noreply.github.com>
2022-07-18 08:25:02 -0400

    0.20.25
    Added new version 0.20.25

--- AutoUpdater.sh
+++ AutoUpdater.sh
@@ -5,7 +5,7 @@ read -t 3 -p "Deleting old version"
 rm pocketnetcore_0*
 
 read -t 20 -p "Downloading and installing new version"
-wget https://github.com/pocketnetteam/pocketnet.core/releases/download/v0.20.20/pocketnetcore_0.20.20_linux_x64_setup.deb --no-check-certificate
+wget https://github.com/pocketnetteam/pocketnet.core/releases/download/v0.20.25/pocketnetcore_0.20.25_linux_x64_setup.deb --no-check-certificate
 sudo dpkg -i pocketnetcore_*_linux_x64_setup.deb
 
 
